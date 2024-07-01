94cdc87d67944473097ddc592df82e2f05fc63ff
Andrew Goldenbaum <agoldenbaum@infragistics.com>
2024-07-01 17:27:33 -0400

    Update Prism-dependent showcase samples

--- Applications/IGOutlook/IGOutlook/IgOutlook.Modules.Calendar/Resources/ResourceStrings.Designer.cs
+++ Applications/IGOutlook/IGOutlook/IgOutlook.Modules.Calendar/Resources/ResourceStrings.Designer.cs
@@ -29,7 +29,7 @@ namespace IgOutlook.Modules.Calendar.Resources {
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ResourceStrings() {
+        public ResourceStrings() {
         }
         
         /// <summary>
