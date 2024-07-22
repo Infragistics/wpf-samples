f2f91d94b03dd7e9baa732d71dd6e6402f175f8c
mddifilippo89 <mdifilippo@infragistics.com>
2024-07-22 15:44:31 -0400

    mdd-data-legend-tooltip-grouping
    mdd-data-legend-tooltip-grouping

--- Examples/IGDataChart/Resources/DataChartStrings.Designer.cs
+++ Examples/IGDataChart/Resources/DataChartStrings.Designer.cs
@@ -29,7 +29,7 @@ namespace IGDataChart.Resources {
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal DataChartStrings() {
+        public DataChartStrings() {
         }
         
         /// <summary>
