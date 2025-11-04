21a8c2aecce8ad46c381fdc7b73a986fcafb33c1
mddifilippo89 <mdifilippo@infragistics.com>
2025-11-04 14:40:31 -0500

    * 'Line 1: Added the using directive for 'Infragistics.Samples.Framework' so that 'SampleContainer' (now 'Infragistics.Samples.Framework.SampleContainer') can be resolved.' in file 'Examples\IGSpreadsheet\Samples\Display\ConfiguringFilterDialog.xaml.cs'

--- Examples/IGSpreadsheet/Samples/Display/ConfiguringFilterDialog.xaml.cs
+++ Examples/IGSpreadsheet/Samples/Display/ConfiguringFilterDialog.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using IGSpreadsheet.Samples.Shared;
+using Infragistics.Samples.Framework;
 using IGSpreadsheet.Shared;
 using Infragistics.Documents.Excel;
 using Infragistics.Documents.Excel.Filtering;
