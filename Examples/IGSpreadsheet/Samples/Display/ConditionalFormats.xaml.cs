0204d5f44d6cd3bb4d9616959af5a0757836f392
mddifilippo89 <mdifilippo@infragistics.com>
2025-11-04 14:42:50 -0500

    * 'Line 17: Added the using directive for IGSpreadsheet.Samples.Shared to resolve the Tools class reference.' in file 'Examples\IGSpreadsheet\Samples\Display\ConditionalFormats.xaml.cs'

--- Examples/IGSpreadsheet/Samples/Display/ConditionalFormats.xaml.cs
+++ Examples/IGSpreadsheet/Samples/Display/ConditionalFormats.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IGSpreadsheet.Samples.Shared;
 
 namespace IGSpreadsheet.Samples.Display
 {
