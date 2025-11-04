e69ef15fbaf3af99bc59d2f7fb36b553ea79c2de
mddifilippo89 <mdifilippo@infragistics.com>
2025-11-04 14:30:41 -0500

    * 'Line 13: The namespace 'Infragistics.Samples.Framework' does not exist in the project or dependencies. The correct namespace for shared sample utilities is 'IGSpreadsheet.Samples.Shared', which is present in the project.' in file 'Examples\IGSpreadsheet\Samples\Display\Formatting.xaml.cs'

--- Examples/IGSpreadsheet/Samples/Display/Formatting.xaml.cs
+++ Examples/IGSpreadsheet/Samples/Display/Formatting.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Infragistics.Samples.Framework;
+using IGSpreadsheet.Samples.Shared;
 using System.IO;
 using IGSpreadsheet.Samples.Shared;
 using Infragistics.Documents.Excel;
