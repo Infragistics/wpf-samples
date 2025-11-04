5a1395891d45e481ad05aa92b60fb969eef46827
mddifilippo89 <mdifilippo@infragistics.com>
2025-11-04 14:31:24 -0500

    * 'Line 6: Removed the using directive for 'Infragistics.Samples.Framework' because the 'Samples' namespace does not exist under 'Infragistics' in the referenced assemblies or packages, which resolves CS0234.' in file 'Examples\IGSpreadsheet\Samples\Editing\ObtainHoveredCell.xaml.cs'

--- Examples/IGSpreadsheet/Samples/Editing/ObtainHoveredCell.xaml.cs
+++ Examples/IGSpreadsheet/Samples/Editing/ObtainHoveredCell.xaml.cs
@@ -3,7 +3,6 @@ using IGSpreadsheet.Samples.Shared;
 using IGSpreadsheet.Shared;
 using Infragistics.Controls.Grids;
 using Infragistics.Documents.Excel;
-using Infragistics.Samples.Framework;
 using System.IO;
 using System.Windows;
 
