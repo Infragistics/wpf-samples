cbd94a6362f5c6c591fd4069fe73e295fe097134
Andrew Goldenbaum <agoldenbaum@infragistics.com>
2025-11-07 16:03:31 -0500

    User Annotation Layer Sample

---
+++ Examples/IGDataChart/Controls/AnnotationDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Infragistics.Controls.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IGDataChart.Controls
+{
+    /// <summary>
+    /// Interaction logic for AnnotationDialog.xaml
+    /// </summary>
+    public partial class AnnotationDialog : Window
+    {
+        public bool ShouldCommit { get; set; }
+        public UserAnnotationInformation AnnotationInformation { get; internal set; }
+
+        public AnnotationDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            ShouldCommit = true;
+            AnnotationInformation.Label = LabelTextBox.Text;
+            AnnotationInformation.AnnotationData = InputTextBox.Text;
+            AnnotationInformation.MainColor = mainColor.Value;
+            AnnotationInformation.BadgeColor = badgeColor.Value;
+
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
