aaa3a4f046077f1d5af32a98dcb9590147a23323
Andrew Goldenbaum <agoldenbaum@infragistics.com>
2025-11-12 17:33:15 -0500

    Update User Annotation Layer sample and SB version

--- Examples/IGDataChart/Samples/Layers/UserAnnotationLayer.xaml.cs
+++ Examples/IGDataChart/Samples/Layers/UserAnnotationLayer.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using IGDataChart.Controls;
 using Infragistics.Controls.Charts;
 using Infragistics.Samples.Framework;
+using Markdig;
+using Microsoft.Web.WebView2.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +28,7 @@ namespace IGDataChart.Samples.Layers
         public UserAnnotationLayer()
         {
             InitializeComponent();
+            UseDefaultTheme = true;
         }
 
         private void Chart_UserAnnotationInformationRequested(object sender, Infragistics.Controls.Charts.UserAnnotationInformationEventArgs args)
@@ -57,11 +61,53 @@ namespace IGDataChart.Samples.Layers
             }
         }
 
-        private void Chart_UserAnnotationToolTipContentUpdating(object sender, Infragistics.Controls.Charts.UserAnnotationToolTipContentUpdatingEventArgs args)
+        private async void Wv_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
         {
-            UserAnnotationInformation info = args.AnnotationInfo as UserAnnotationInformation;
-            ContentControl container = (ContentControl)args.Content;
-            container.Content = info.AnnotationData;
+            var wv = (WebView2)sender;
+            var tc = "white";
+            await wv.CoreWebView2.ExecuteScriptAsync("document.body.style.color = '" + tc + "'");
+        }
+
+        private async void Chart_UserAnnotationToolTipContentUpdating(object sender, Infragistics.Controls.Charts.UserAnnotationToolTipContentUpdatingEventArgs args)
+        {
+            var cc = (ContentControl)args.Content;
+            Grid g = null;
+            WebView2 wv = null;
+
+            if (cc.Content == null)
+            {
+                g = new Grid();
+                g.Width = 150;
+                g.Height = 200;
+                cc.Content = g;
+
+                wv = new WebView2();
+                wv.NavigationCompleted += Wv_NavigationCompleted;
+                g.Children.Add(wv);
+
+            }
+            else
+            {
+                g = (Grid)cc.Content;
+                wv = (WebView2)g.Children[0];
+            }
+
+            await wv.EnsureCoreWebView2Async();
+
+            var html = Markdown.ToHtml(args.AnnotationInfo.AnnotationData);
+
+            var info = args.AnnotationInfo;
+            var bg = info.MainColor;
+
+
+
+            html = Regex.Replace(html, "<a ", "<a target=\"_blank\" ");
+
+            if (!html.Equals(wv.Tag))
+            {
+                wv.Tag = html;
+                wv.NavigateToString(html);
+            }
         }
     }
 }
